30c228a97398162869b8c123671fd5a3207a57a2
Ritesh Pandit <134190903+R17358@users.noreply.github.com>
2026-02-01 17:21:39 +0530

    Add seller and admin routes for orders and suborders

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -4,26 +4,64 @@ const {
   getSingleOrder,
   myOrders,
   getAllOrders,
-  updateOrder,
+  getAllSubOrders,
+  updateSellerSubOrder,
   deleteOrder,
+  getSellerOrders,
+  getSellerSubOrder,
+  processSellerPayment,
 } = require("../controllers/orderController");
-const router = express.Router();
 
+const router = express.Router();
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
+// Customer routes
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
-
 router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
-
 router.route("/orders/me").get(isAuthenticatedUser, myOrders);
 
-router
-  .route("/admin/orders")
-  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+// Seller routes
+router.route("/seller/orders").get(
+  isAuthenticatedUser,
+  authorizeRoles("seller"),
+  getSellerOrders
+);
 
-router
-  .route("/admin/order/:id")
-  .put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder)
-  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
+router.route("/seller/order/:id").get(
+  isAuthenticatedUser,
+  authorizeRoles("seller"),
+  getSellerSubOrder
+);
+
+router.route("/seller/order/:id").put(
+  isAuthenticatedUser,
+  authorizeRoles("seller"),
+  updateSellerSubOrder
+);
+
+// Admin routes
+router.route("/admin/orders").get(
+  isAuthenticatedUser,
+  authorizeRoles("admin"),
+  getAllOrders
+);
+
+router.route("/admin/suborders").get(
+  isAuthenticatedUser,
+  authorizeRoles("admin"),
+  getAllSubOrders
+);
+
+router.route("/admin/order/:id").delete(
+  isAuthenticatedUser,
+  authorizeRoles("admin"),
+  deleteOrder
+);
+
+router.route("/admin/suborder/:id/pay").put(
+  isAuthenticatedUser,
+  authorizeRoles("admin"),
+  processSellerPayment
+);
 
 module.exports = router;
